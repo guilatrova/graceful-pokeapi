053f57576c75dfab68f5efd9db69cc5fc4fc7524
guilatrova <hello@guilatrova.dev>
2023-12-02 17:59:30 -0300

    refactor(typing): use optional

--- src/gpokeapi/clients/contests.py
+++ src/gpokeapi/clients/contests.py
@@ -1,20 +1,19 @@
 import typing as t
 
-from gracy import GracyNamespace, parsed_response
-
 from gpokeapi.endpoints import PokeApiEndpoint
 from gpokeapi.models import contests as models
+from gracy import GracyNamespace, parsed_response
 
 
 class ContestNamespace(GracyNamespace[PokeApiEndpoint]):
-    @parsed_response(t.Union[models.ContestType, None])
+    @parsed_response(t.Optional[models.ContestType])
     async def get_type(self, name_or_id: t.Union[str, int]):
         return await self.get(PokeApiEndpoint.CONTEST_TYPE, dict(KEY=str(name_or_id)))
 
-    @parsed_response(t.Union[models.ContestEffect, None])
+    @parsed_response(t.Optional[models.ContestEffect])
     async def get_effect(self, name_or_id: t.Union[str, int]):
         return await self.get(PokeApiEndpoint.CONTEST_EFFECT, dict(KEY=str(name_or_id)))
 
-    @parsed_response(t.Union[models.SuperContestEffect, None])
+    @parsed_response(t.Optional[models.SuperContestEffect])
     async def get_super_effect(self, name_or_id: t.Union[str, int]):
         return await self.get(PokeApiEndpoint.CONTEST_SUPER_EFFECT, dict(KEY=str(name_or_id)))
